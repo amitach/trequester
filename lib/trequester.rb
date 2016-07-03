523c1475d9c1e3a915b5fde59b026443f4c8949c
Amit Acharya <amit@nestaway.com>
2016-07-03 14:03:38 +0530

    Adding the code to make get requests

--- lib/trequester.rb
+++ lib/trequester.rb
@@ -1,5 +1,24 @@
 require "trequester/version"
+require 'httparty'
 
 module Trequester
-  # Your code goes here...
+  class Request
+    include HTTParty
+    #initialized the base URL,
+    #Since we are including the HTTParty module the base_uri method is available as a method on the eigen class
+    def initialize(root_endpoint)
+      @root_endpoint = root_endpoint
+      self.class.base_uri(root_endpoint)
+    end
+
+    #To be used primarily for get requests
+    def fetch(rest_of_the_url)
+      request :get, "#{@root_endpoint}#{@entity_name}/#{rest_of_the_url}"
+    end
+
+    #make the actual request
+    def request(method, url, data = {})
+      self.class.send(method, url, query: data)
+    end
+  end
 end
